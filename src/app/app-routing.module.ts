ff6a7a5ddb3d25932e7efddd26e78aea3a01d79d
Maud <maud.chevalier15@gmail.com>
2023-01-04 10:11:21 +0100

    Routing when logged in

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -6,6 +6,8 @@ import {SignupComponent} from "./components/signup/signup.component";
 
 const routes: Routes = [
     {path: '', redirectTo: 'home', pathMatch: 'full'},
+    //TODO : remove when home is implemented
+    {path: 'home', redirectTo: 'signup', pathMatch: 'full'},
     {path: 'login', component: LoginComponent},
     {path: 'signup', component: SignupComponent},
     // {path: 'home', component: MyComponent, canActivate: [IsSigninGuardService]},
