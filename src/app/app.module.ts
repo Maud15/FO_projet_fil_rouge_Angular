183a7a28c23e49f8134421b59f9ebdba015e29b6
Maud <maud.chevalier15@gmail.com>
2023-01-04 10:05:59 +0100

    Init angular material

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,19 @@ import {BrowserModule} from '@angular/platform-browser';
 
 import {HttpClientModule} from "@angular/common/http";
 import {AppRoutingModule} from './app-routing.module';
-import {AppComponent} from './app.component';
-import { LoginComponent } from './components/login/login.component';
-import {AuthInterceptorProviders} from "./helpers/auth.interceptor";
 import {ReactiveFormsModule} from "@angular/forms";
-import { SignupComponent } from './components/signup/signup.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
 import {MatInputModule} from '@angular/material/input';
 import {MatFormFieldModule} from '@angular/material/form-field';
 import {MatButtonModule} from '@angular/material/button';
 import {MatIconModule} from '@angular/material/icon';
+
+import {AuthInterceptorProviders} from "./helpers/auth.interceptor";
+
+import {AppComponent} from './app.component';
+import { LoginComponent } from './components/login/login.component';
+import { SignupComponent } from './components/signup/signup.component';
 
 @NgModule({
     declarations: [
